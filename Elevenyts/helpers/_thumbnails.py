1f390229f148ed68cd1c4c3b5dbaa0dbd5842145
6399161357 <jay990x@gmail.com>
2026-06-17 20:27:27 +0530

    Update _thumbnails.py

--- Elevenyts/helpers/_thumbnails.py
+++ Elevenyts/helpers/_thumbnails.py
@@ -56,13 +56,8 @@ ICONS_Y = BAR_Y + 65
 
 MAX_TITLE_WIDTH = 850
 
-_f = "QXJ0aXN0Ym90cw=="
-
-
 def _decode_f():
-    decoded = base64.b64decode(_f).decode("utf-8")
-    return f"✦ {decoded} ✦"
-
+    return "L ɪ ɢ ʜ ᴛ"
 
 def trim_to_width(text: str, font, max_w: int) -> str:
 
